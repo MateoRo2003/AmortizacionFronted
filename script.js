99614591e3454bf8c4db63ff206337055dc24de1
Mateo Rojas <mateorojasarce2003@gmail.com>
2025-11-10 14:45:15 -0300

    wep

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ async function recalcularConSistema(sistema) {
         // Retraso de 2 segundos
         await new Promise(resolve => setTimeout(resolve, 1000));
 
-        const res = await fetch("https://amortizacionbackend.onrender.com/api/calcular", {
+        const res = await fetch("https://amortizacionbackend-production.up.railway.app//api/calcular", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(payload)
@@ -87,7 +87,7 @@ async function actualizarComparacionConSistema(sistemaPrincipal) {
     };
 
     try {
-        const res = await fetch("https://amortizacionbackend.onrender.com/api/calcular", {
+        const res = await fetch("https://amortizacionbackend-production.up.railway.app//api/calcular", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(payload)
@@ -171,7 +171,7 @@ async function calcularYActualizar() {
         const controller = new AbortController();
         const timeoutId = setTimeout(() => controller.abort(), 45000); // 45 segundos
 
-        const res = await fetch("https://amortizacionbackend.onrender.com/api/calcular", {
+        const res = await fetch("https://amortizacionbackend-production.up.railway.app//api/calcular", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(payload),
@@ -284,7 +284,7 @@ async function compararBancos() {
         // Retraso de 2 segundos
         await new Promise(resolve => setTimeout(resolve, 1000));
 
-        const res = await fetch("https://amortizacionbackend.onrender.com/api/calcular", {
+        const res = await fetch("https://amortizacionbackend-production.up.railway.app//api/calcular", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(payload)
